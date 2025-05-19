2b7ebd9e4ae1e4e9eaedea6fdfb4f9438c8aed77
paco <ernestogutierrez175@gmail.com>
2025-05-19 09:38:10 -0400

    paco commit

--- login/login_index.php
+++ login/login_index.php
@@ -24,15 +24,22 @@
     
     </div>
     <a href="../landing/index.php" class="back-arrow" title="Volver a la página principal">&#8592; Volver a la página principal</a>
-    <form id="login_form" action="login.php" method="POST">
-        <h2 style="text-align:center; margin-bottom: 24px; ">Login</h2>
-        <label for="email">Email</label>
-        <input type="text" id="email" name="email" placeholder="Ingrese su correo electronico"><br>
+    <div>
+        <form id="login_form" action="login.php" method="POST">
+            <h2 style="text-align:center; margin-bottom: 24px; ">Login</h2>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" placeholder="Ingrese su correo electronico"><br>
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Ingrese su clave"><br>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Ingrese su clave"><br>
 
-        <button type="submit" class="submit_button">Iniciar sesion</button><br><br>
-    </form>
+            <button type="submit" class="submit_button">Iniciar sesion</button>
+        </form>
+       <a href="../sign_in/registro_index.php" style="display: block; margin-top: 16px; text-align: center;">
+        <button type="button" class="submit_button" style="width: 100%;">Registrarse</button>
+    </a>
+    </div>
+    
+
 </body>
 </html>
