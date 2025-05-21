3b8cb73a1f5db5d1696e4e89b64d925dfe1e6dba
paco <ernestogutierrez175@gmail.com>
2025-05-20 21:49:41 -0400

    commit

--- admin/validar_admin.php
+++ admin/validar_admin.php
@@ -1,7 +1,6 @@
 <?php
 function validar_admin(){
     #validacion de que el que ingrese a este modulo sea un adminitrador
-    session_start();
     if($_SESSION['tipo'] != 1){
         header('location: ../cerrar_sesion.php');
         exit();
--- login/login_index.php
+++ login/login_index.php
@@ -5,30 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../estilos/estilos_login.css">
-<<<<<<< HEAD
-    <style>
-        .back-arrow {
-            display: inline-block;
-            margin-bottom: 16px;
-            font-size: 24px;
-            text-decoration: none;
-            color: #4a90e2;
-            transition: color 0.2s;
-        }
-    </style>
-=======
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
->>>>>>> 512f4700a00789609126cf66a7fae57dd4ff4424
 </head>
 <body>
     <div class="pantalla_verde">
     
     </div>
-<<<<<<< HEAD
-    <a href="../landing/index.php" class="back-arrow" title="Volver a la página principal">Volver a la página principal</a>
-    <div>
-        <form id="login_form" action="login.php" method="POST">
-=======
     
     <div class="volver">
 
@@ -40,7 +22,6 @@
      </div>
     <div id="login_form">
         <form  action="login.php" method="POST">
->>>>>>> 512f4700a00789609126cf66a7fae57dd4ff4424
             <h2 style="text-align:center; margin-bottom: 24px; ">Login</h2>
             <label for="email">Email</label>
             <input type="text" id="email" name="email" placeholder="Ingrese su correo electronico"><br>
