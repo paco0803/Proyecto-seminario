f731a170dca1cb2519eec8cb8d588d57383f3650
Eloy Rengel <eloypanita@gmail.com>
2025-06-07 12:31:28 -0400

    asdasd

--- landing/detalle_producto.php
+++ landing/detalle_producto.php
@@ -37,7 +37,7 @@ if(isset($_SESSION['tipo']) && $_SESSION['tipo'] == 2){
     <title>Document</title>
     <link rel="stylesheet" href="../estilos/estilos_landig.css">
     <link rel="stylesheet" href="../estilos/estilos_producto_detalle.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <header>
